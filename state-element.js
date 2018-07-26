4007d964b9490145d4d03ec07fe8791ee2671f02
ludeeus <joasoe@gmail.com>
2018-07-26 11:57:55 +0200

    uploaded correct file

--- state-element.js
+++ state-element.js
@@ -1,11 +1,10 @@
-class StateAttributeElement extends HTMLElement {
+class StateElement extends HTMLElement {
   set hass(hass) {
     const entityId = this.config.entity;
     const prefix_string = this.config.prefix
     const show_empty = this.config.show_empty
-    const attr = this.config.attribute;
-    const state = hass.states[entityId].attributes[attr];
-    const card = document.createElement('state-attribute-element');
+    const state = hass.states[entityId].state;
+    const card = document.createElement('state-element');
     if (state.length != 0) {
       if (prefix_string) {
         this.innerHTML = prefix_string + state;
@@ -24,9 +23,6 @@ class StateAttributeElement extends HTMLElement {
     if (!config.entity) {
       throw new Error('You need to define an entity');
     }
-    if (!config.attribute) {
-      throw new Error('You need to define an attribute');
-    }
     if (!config.show_empty) {
       config.show_empty = false;
     }
@@ -37,4 +33,4 @@ class StateAttributeElement extends HTMLElement {
     return 1;
   }
 }
-customElements.define('state-attribute-element', StateAttributeElement);
+customElements.define('state-element', StateElement);
